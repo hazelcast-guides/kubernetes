27ae4d2c8ecf82e82a2487f4a8fab9ca6b97921c
Rafal Leszko <rafal@hazelcast.com>
2020-12-10 15:33:04 +0100

    Update after code review

--- nodejs/client.js
+++ nodejs/client.js
@@ -14,16 +14,16 @@ const clientConfig = {
     try {
         const client = await Client.newHazelcastClient(clientConfig);
         const map = await client.getMap('map');
-        await map.put("key", "value")
-        const res = await map.get("key")
-        if (res !== "value") {
-            throw new Error("Connection failed, check your configuration.")
+        await map.put('key', 'value');
+        const res = await map.get('key');
+        if (res !== 'value') {
+            throw new Error('Connection failed, check your configuration.');
         }
-        console.log("Connection Successful!");
-        console.log("Now, `map` will be filled with random entries.");
+        console.log('Connection Successful!');
+        console.log('Now, `map` will be filled with random entries.');
         while (true) {
             const randomKey = Math.floor(Math.random() * 100000);
-            await map.put('key' + randomKey, 'value' + randomKey)
+            await map.put('key' + randomKey, 'value' + randomKey);
             await map.get('key' + randomKey);
             if (randomKey % 100 === 0) {
                 map.size().then((size) => console.log(`map size: ${size}`));
