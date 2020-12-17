18e7911180a9b5bcd70d4fe1070e0ba90bed3942
Rafal Leszko <rafal@hazelcast.com>
2020-12-17 19:47:27 +0100

    Update Hazelast CPP Client

--- cpp/Client.cpp
+++ cpp/Client.cpp
@@ -7,7 +7,8 @@
 
 int main() {
     hazelcast::client::client_config config;
-    config.get_network_config().add_address(address("hz-hazelcast", 5701));    
+    config.get_network_config().add_address(address("hz-hazelcast", 5701));
+    config.get_network_config().set_connection_attempt_limit(INT32_MAX);
     hazelcast::client::hazelcast_client hz(std::move(config));
 
     std::cout << "Successful connection!" << std::endl;
@@ -18,12 +19,12 @@ int main() {
         int random_key = rand() % 100000;
         try {
             map->put("key-" + std::to_string(random_key), "value-" + std::to_string(random_key));
+            if (random_key % 100 == 0) {
+                std::cout << "Current map size: " + std::to_string(map->size().get()) << std::endl;
+                std::this_thread::sleep_for(std::chrono::seconds(1));
+            }
         } catch (const std::exception& e) {
             std::cout << e.what() << std::endl;
-        }
-        if (random_key % 100 == 0) {
-            std::cout << "Current map size: " + std::to_string(map->size().get()) << std::endl;
-            std::this_thread::sleep_for(std::chrono::seconds(1));
         }
     }
 
