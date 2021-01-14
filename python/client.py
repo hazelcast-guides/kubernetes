22f8145b8e4fcf8905d02d5d02130e3396e32a91
Rafal Leszko <rafal@hazelcast.com>
2021-01-14 10:28:40 +0100

    Update after code review

--- python/client.py
+++ python/client.py
@@ -2,34 +2,31 @@ import hazelcast
 import logging
 import random
 import time
-import traceback
 
-if __name__ == "__main__":
-    logging.basicConfig()
-    logging.getLogger().setLevel(logging.INFO)
+logging.basicConfig(level=logging.INFO)
 
-    client = hazelcast.HazelcastClient(
-        cluster_members=["hz-hazelcast"]
-    )
+client = hazelcast.HazelcastClient(
+    cluster_members=["hz-hazelcast"],
+)
 
-    my_map = client.get_map("map").blocking()
-    my_map.put("key", "value")
+my_map = client.get_map("map").blocking()
+my_map.put("key", "value")
 
-    if my_map.get("key") == "value":
-        print("Successful connection!")
-        print("Starting to fill the map with random entries.");
-        while True:
-            random_key = random.randint(1, 100000)
-            try:
-                my_map.put("key" + str(random_key), "value" + str(random_key))
-            except Exception as err:
-                traceback.print_tb(err.__traceback__)
+if my_map.get("key") == "value":
+    print("Successful connection!")
+    print("Starting to fill the map with random entries.")
 
-            if random_key % 100 == 0:
-                print("Current map size: " + str(my_map.size()))
-                time.sleep(1)
+    while True:
+        random_key = random.randint(1, 100000)
+        random_key_str = str(random_key)
+        try:
+            my_map.put("key" + random_key_str, "value" + random_key_str)
+        except:
+            logging.exception("Put operation failed!")
 
-    else:
-        raise Exception("Connection failed, check your configuration.")
-
+        if random_key % 100 == 0:
+            print("Current map size:", my_map.size())
+            time.sleep(1)
+else:
     client.shutdown()
+    raise Exception("Connection failed, check your configuration.")
